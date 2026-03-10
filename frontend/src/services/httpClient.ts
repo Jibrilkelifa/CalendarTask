60e989dd90a934796796204e5c6179da78500d69
Jibril Kalifa <120446317+Jibrilkelifa@users.noreply.github.com>
2026-03-10 11:03:34 +0300

    feat: edited httpClient

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -1,9 +1,10 @@
 import axios from "axios"
 
 export const http = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,   
   headers: {
     "Content-Type": "application/json"
   }
 })
+
 
