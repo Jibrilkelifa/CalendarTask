51f17611b2e6e32a7c8e34b3d81d75bc57feae93
Jibril Kalifa <120446317+Jibrilkelifa@users.noreply.github.com>
2026-03-10 11:21:30 +0300

    feat:CORS added on backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,14 @@ const errorMiddleware = require("./middlewares/error.middleware")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                 
+    "https://calendar-task-seven.vercel.app" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use(express.json())
 
 app.use("/tasks", taskRoutes)
